9b146695ab48eed28d8c8d8990c402de5e7b33d5
badhon-dalbot <bdalbot116@gmail.com>
2024-04-22 00:53:54 +0600

    Linked List Operations: insert at end

--- linkList/linkedListOperations.cpp
+++ linkList/linkedListOperations.cpp
@@ -6,30 +6,53 @@ struct Node
     int data;
     struct Node *next;
 };
+
 void insertAtBeginning(struct Node **head, int newData)
 {
-    struct Node* newNode = new Node();
+    struct Node *newNode = new Node();
     newNode->data = newData;
     newNode->next = *head;
 
     *head = newNode;
 }
 
-void printLinkList(struct Node* head)
+void insertAtEnd(struct Node **head, int newData)
+{
+    struct Node *newNode = new Node();
+    struct Node *last = *head;
+
+    newNode->data = newData;
+    newNode->next = NULL;
+
+    if (*head == NULL)
+    {
+        *head = newNode;
+        return;
+    }
+
+    while (last->next != NULL)
+    {
+        last = last->next;
+    }
+    last->next = newNode;
+    return;
+}
+
+void printLinkList(struct Node *head)
 {
     while (head != NULL)
     {
-        cout << head->data<<" ";
+        cout << head->data << " ";
         head = head->next;
     }
 }
 
-
 int main()
 {
-    struct Node* head = NULL;
+    struct Node *head = NULL;
     insertAtBeginning(&head, 4);
     insertAtBeginning(&head, 5);
+    insertAtEnd(&head, 7);
 
     printLinkList(head);
     return 0;
