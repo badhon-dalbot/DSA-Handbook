f5b73b2ed5f602fb0444ccdf76c6b902c486c5d9
badhon-dalbot <bdalbot116@gmail.com>
2024-05-01 19:43:10 +0600

    Binary Tree: inseart

--- tree/binaryTree.cpp
+++ tree/binaryTree.cpp
@@ -15,11 +15,29 @@ struct Node *newNode(int x)
     return node;
 }
 
-struct Node *search(struct Node *root, int key){
-    if(root == NULL || root->value == key)
-    return root;
+struct Node *insert(struct Node *root, int value)
+{
+    if (root == NULL)
+    {
+        return newNode(value);
+    }
+    if (value < root->value)
+    {
+        root->left = insert(root->left, value);
+    }
+    else if (root->value < value)
+    {
+        root->right = insert(root->right, value);
+    }
+}
 
-    if(root->value < key){
+struct Node *search(struct Node *root, int key)
+{
+    if (root == NULL || root->value == key)
+        return root;
+
+    if (root->value < key)
+    {
         return search(root->right, key);
     }
     return search(root->left, key);
@@ -27,16 +45,15 @@ struct Node *search(struct Node *root, int key){
 int main()
 {
     struct Node *root = NULL;
-    root = newNode(1);
-    root->left = newNode(2);
-    root->right = newNode(3);
-    root->left->left = newNode(4);
-    root->left->right = newNode(5);
-    root->left->right->left = newNode(6);
-    root->left->right->right = newNode(7);
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 70);
+    insert(root, 60);
+    insert(root, 80);
 
-    (search(root, 8) != NULL) ? 
-    cout<<"Found": cout<<"Not Found";
+    (search(root, 80) != NULL) ? cout << "Found" : cout << "Not Found";
 
     return 0;
 }
